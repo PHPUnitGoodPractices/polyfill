71064199f049378776a1e4b432d848fc097726ef
Alexey Kopytko <alexey@kopytko.com>
2020-11-19 09:43:34 +0900

    Try to call parent at all times

--- src/PolyfillTrait6.php
+++ src/PolyfillTrait6.php
@@ -38,46 +38,82 @@ trait PolyfillTrait6
 
     public static function assertIsArray($actual, $message = '')
     {
-        static::assertInternalType('array', $actual, $message);
+        if (\is_callable(['parent', 'assertIsArray'])) {
+            parent::assertIsArray($actual, $message);
+        } else {
+            static::assertInternalType('array', $actual, $message);
+        }
     }
 
     public static function assertIsString($actual, $message = '')
     {
-        static::assertInternalType('string', $actual, $message);
+        if (\is_callable(['parent', 'assertIsString'])) {
+            parent::assertIsString($actual, $message);
+        } else {
+            static::assertInternalType('string', $actual, $message);
+        }
     }
 
     public static function assertIsBool($actual, $message = '')
     {
-        static::assertInternalType('bool', $actual, $message);
+        if (\is_callable(['parent', 'assertIsBool'])) {
+            parent::assertIsBool($actual, $message);
+        } else {
+            static::assertInternalType('bool', $actual, $message);
+        }
     }
 
     public static function assertIsCallable($actual, $message = '')
     {
-        static::assertInternalType('callable', $actual, $message);
+        if (\is_callable(['parent', 'assertIsCallable'])) {
+            parent::assertIsCallable($actual, $message);
+        } else {
+            static::assertInternalType('callable', $actual, $message);
+        }
     }
 
     public static function assertIsInt($actual, $message = '')
     {
-        static::assertInternalType('int', $actual, $message);
+        if (\is_callable(['parent', 'assertIsInt'])) {
+            parent::assertIsInt($actual, $message);
+        } else {
+            static::assertInternalType('int', $actual, $message);
+        }
     }
 
     public static function assertMatchesRegularExpression($pattern, $string, $message = '')
     {
-        static::assertRegExp($pattern, $string, $message);
+        if (\is_callable(['parent', 'assertMatchesRegularExpression'])) {
+            parent::assertMatchesRegularExpression($pattern, $string, $message);
+        } else {
+            static::assertRegExp($pattern, $string, $message);
+        }
     }
 
     public static function assertStringContainsString($needle, $haystack, $message = '')
     {
-        static::assertContains($needle, $haystack, $message);
+        if (\is_callable(['parent', 'assertStringContainsString'])) {
+            parent::assertStringContainsString($needle, $haystack, $message);
+        } else {
+            static::assertContains($needle, $haystack, $message);
+        }
     }
 
     public static function assertStringNotContainsString($needle, $haystack, $message = '')
     {
-        static::assertNotContains($needle, $haystack, $message);
+        if (\is_callable(['parent', 'assertStringNotContainsString'])) {
+            parent::assertStringNotContainsString($needle, $haystack, $message);
+        } else {
+            static::assertNotContains($needle, $haystack, $message);
+        }
     }
 
     public static function assertFileDoesNotExist($filename, $message = '')
     {
-        static::assertFileNotExists($filename, $message);
+        if (\is_callable(['parent', 'assertFileDoesNotExist'])) {
+            parent::assertFileDoesNotExist($filename, $message);
+        } else {
+            static::assertFileNotExists($filename, $message);
+        }
     }
 }
