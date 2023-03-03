099c8d496b617e24a173559a332e3261d2ca8cae
Kuba Werłos <werlos@gmail.com>
2023-03-03 23:16:54 +0100

    Add PHPUnit 10 support

--- src/aliases.php
+++ src/aliases.php
@@ -9,18 +9,13 @@
  * with this source code in the file LICENSE.
  */
 
-if (!class_exists('PHPUnit\Framework\Error\Warning')) {
-    class_alias('PHPUnit_Framework_Error_Warning', 'PHPUnit\Framework\Error\Warning');
-}
-
-if (!class_exists('PHPUnit\Framework\ExpectationFailedException')) {
-    class_alias('PHPUnit_Framework_ExpectationFailedException', 'PHPUnit\Framework\ExpectationFailedException');
-}
-
-if (!class_exists('PHPUnit\Framework\TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
-}
-
-if (!class_exists('PHPUnit\Runner\Version')) {
-    class_alias('PHPUnit_Runner_Version', 'PHPUnit\Runner\Version');
+foreach ([
+    'PHPUnit_Framework_Error_Warning' => 'PHPUnit\Framework\Error\Warning',
+    'PHPUnit_Framework_ExpectationFailedException' => 'PHPUnit\Framework\ExpectationFailedException',
+    'PHPUnit_Framework_TestCase' => 'PHPUnit\Framework\TestCase',
+    'PHPUnit_Runner_Version' => 'PHPUnit\Runner\Version',
+] as $new => $old) {
+    if (!class_exists($new) && class_exists($old)) {
+        class_alias($old, $new);
+    }
 }
